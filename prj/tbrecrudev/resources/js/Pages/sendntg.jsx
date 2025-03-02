ad15a67376883978725f937c9ecde63c9f396b17
rehoiujghe.ijkvhlikwjfgo'iq1u1o42342 <103235468+GaserVolum@users.noreply.github.com>
2025-03-02 16:58:53 +0700

    ФИНАЛЬНЫЙ ФИНАЛ

--- prj/tbrecrudev/resources/js/Pages/sendntg.jsx
+++ prj/tbrecrudev/resources/js/Pages/sendntg.jsx
@@ -1,4 +1,12 @@
- function redirectToExample() {
+const socket = new WebSocket('ws://localhost:8765');
+
+socket.onopen = () => {
+    console.log('WebSocket соединение установлено');
+    socket.send('НА САЙТЕ НОВОЕ СОБЫТИЕ');
+};
+
+
+function redirectToExample() {
      window.location.href = 'http://localhost:8000/main';
  }
 
